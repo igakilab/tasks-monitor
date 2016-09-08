56c885af417950a5e4a78ea0b45be3510bf64ca8
Koike Takaaki <taka.vao.321@gmail.com>
2016-09-08 15:54:08 +0900

    tomcatのやつにcronをいれたで

---
+++ src/jp/ac/oit/igakilab/tasks/servlet/CronTasks.java
@@ -0,0 +1,26 @@
+package jp.ac.oit.igakilab.tasks.servlet;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import jp.ac.oit.igakilab.marsh.util.LogRecorder;
+import jp.ac.oit.igakilab.tasks.cron.SampleCron;
+
+public class CronTasks implements ServletContextListener{
+	LogRecorder logger;
+	SampleCron cron;
+
+	public CronTasks(){
+		logger = new LogRecorder("cron-test.txt", true);
+		cron = new SampleCron();
+	}
+
+	public void contextInitialized(ServletContextEvent event){
+		logger.addSingleLog("Cron Initialization.", true);
+		cron.schedulerSimple();
+	}
+
+	public void contextDestroyed(ServletContextEvent event){
+		logger.addSingleLog("Server shutdown.", true);
+	}
+}
