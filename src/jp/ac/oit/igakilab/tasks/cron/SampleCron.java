aca6cf9878287bd838328cc4270635fe247a9578
Koike Takaaki <taka.vao.321@gmail.com>
2016-09-08 14:54:24 +0900

    cronのライブラリ登録、動作をテスト

---
+++ src/jp/ac/oit/igakilab/tasks/cron/SampleCron.java
@@ -0,0 +1,31 @@
+package jp.ac.oit.igakilab.tasks.cron;
+
+import java.util.Date;
+
+import it.sauronsoftware.cron4j.Scheduler;
+
+public class SampleCron {
+	static public class CronTask implements Runnable {
+		@Override
+		public void run(){
+			System.out.println(new Date() + " : Hello cron4j");
+		}
+	}
+
+	public static void main(String[] args){
+		SampleCron app = new SampleCron();
+		try{
+			app.schedulerSimple();
+			System.out.println("Press Ctrl+C to stop.");
+			Thread.sleep(100000000);
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void schedulerSimple(){
+		Scheduler scheduler = new Scheduler();
+		scheduler.schedule("* * * * *", new CronTask()); //every minute
+		scheduler.start();
+	}
+}
