28f3477bc0e235744e0a7d167878c757240551b1
Koike Takaaki <taka.vao.321@gmail.com>
2016-09-08 16:27:15 +0900

    ごみ製造マシーンを作成

---
+++ src/jp/ac/oit/igakilab/tasks/cron/GarbageMakingMachine.java
@@ -0,0 +1,40 @@
+package jp.ac.oit.igakilab.tasks.cron;
+
+import java.util.Date;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+
+import it.sauronsoftware.cron4j.Scheduler;
+
+public class GarbageMakingMachine implements Runnable{
+	public static String DB_HOST = "150.89.234.253";
+	public static int DB_PORT = 27017;
+	public static String DB_NAME = "tasks";
+	public static String DB_COLLECTION = "garbage";
+
+	public static String[] MEMBERS = {"kiichi", "hayashi", "gida", "shinya", "zoe",
+		"mitsui", "kitaba", "ryokun", "dekopon", "ueyama", "shimizu"};
+
+	public static Scheduler createScheduler(String schedule){
+		Scheduler scheduler = new Scheduler();
+		scheduler.schedule(schedule, new GarbageMakingMachine());
+		return scheduler;
+	}
+
+
+	public void run(){
+		MongoClient client = new MongoClient(DB_HOST, DB_PORT);
+		MongoCollection<Document> collection =
+			client.getDatabase(DB_NAME).getCollection(DB_COLLECTION);
+
+		Document doc = new Document();
+		doc.append("time", new Date())
+			.append("choice", MEMBERS[(int)(Math.random() * MEMBERS.length)]);
+		collection.insertOne(doc);
+
+		client.close();
+	}
+}
--- src/jp/ac/oit/igakilab/tasks/servlet/CronTasks.java
+++ src/jp/ac/oit/igakilab/tasks/servlet/CronTasks.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import jp.ac.oit.igakilab.marsh.util.LogRecorder;
+import jp.ac.oit.igakilab.tasks.cron.GarbageMakingMachine;
 import jp.ac.oit.igakilab.tasks.cron.SampleCron;
 
 public class CronTasks implements ServletContextListener{
@@ -18,6 +19,7 @@ public class CronTasks implements ServletContextListener{
 	public void contextInitialized(ServletContextEvent event){
 		logger.addSingleLog("Cron Initialization.", true);
 		cron.schedulerSimple();
+		GarbageMakingMachine.createScheduler("* * * * 33").start();
 	}
 
 	public void contextDestroyed(ServletContextEvent event){
