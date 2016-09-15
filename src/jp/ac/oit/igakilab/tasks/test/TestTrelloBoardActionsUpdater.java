ae11307fa55134bb7e3200c76fc3b09ace1b28a3
koike-marshmallow <taka.vao.321@gmail.com>
2016-09-15 21:35:02 +0900

    エラーを修正

--- src/jp/ac/oit/igakilab/tasks/test/TestTrelloBoardActionsUpdater.java
+++ src/jp/ac/oit/igakilab/tasks/test/TestTrelloBoardActionsUpdater.java
@@ -69,7 +69,6 @@ public class TestTrelloBoardActionsUpdater {
 	}
 
 	static void initProperties(){
-		AppProperties.globalInit();
 		AppProperties.globalSet("tasks.trello.key", "67ad72d3feb45f7a0a0b3c8e1467ac0b");
 		AppProperties.globalSet("tasks.trello.token",
 			"268c74e1d0d1c816558655dbe438bb77bcec6a9cd205058b85340b3f8938fd65");
