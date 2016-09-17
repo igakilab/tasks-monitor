cb8555520c0ec5084d07f6990a16faa0055c9bf9
kioke takaaki <taka.vao.321@gmail.com>
2016-09-17 19:27:12 +0900

    とちゅー

---
+++ src/jp/ac/oit/igakilab/tasks/test/TestMembersDB.java
@@ -0,0 +1,20 @@
+package jp.ac.oit.igakilab.tasks.test;
+
+import com.mongodb.MongoClient;
+
+import jp.ac.oit.igakilab.tasks.db.MembersDB;
+import jp.ac.oit.igakilab.tasks.db.TasksMongoClientBuilder;
+import jp.ac.oit.igakilab.tasks.members.Member;
+
+public class TestMembersDB {
+	public static void main(String[] args){
+		MongoClient client = TasksMongoClientBuilder.createClient();
+		MembersDB mdb = new MembersDB(client);
+
+		Member member = new Member("koike");
+		member.setName("koike");
+		member.setSlackId("koike");
+		
+		//mdb.addMember(member, new MemberDocumentConverter());
+	}
+}
