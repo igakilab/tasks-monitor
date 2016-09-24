af2bac916dfb9c29f31c763c949f86f265ca53a3
koike-marshmallow <taka.vao.321@gmail.com>
2016-09-24 13:31:08 +0900

    validPeriodを追加

--- src/jp/ac/oit/igakilab/tasks/db/SprintsDB.java
+++ src/jp/ac/oit/igakilab/tasks/db/SprintsDB.java
@@ -1,6 +1,9 @@
 package jp.ac.oit.igakilab.tasks.db;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.bson.Document;
@@ -9,11 +12,34 @@ import org.bson.conversions.Bson;
 import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
 
 import jp.ac.oit.igakilab.tasks.util.RandomIdGenerator;
 
 public class SprintsDB {
+	public static void main(String[] args){
+		MongoClient client = TasksMongoClientBuilder.createClient();
+		SprintsDB sdb = new SprintsDB(client);
+		/*Sprint spr = new Sprint();
+		spr.setBoardId("52e");
+		Calendar cal = Calendar.getInstance();
+		cal.set(2016, 8, 8);
+		spr.setBeginDate(cal.getTime());
+		cal.set(2016, 8, 15);
+		spr.setFinishDate(cal.getTime());
+		sdb.addSprint(spr, new SprintDocumentConverter());*/
+		Calendar begin = Calendar.getInstance();
+		begin.set(2016, 8, 15);
+		Calendar finish = Calendar.getInstance();
+		finish.set(2016, 8, 22);
+		boolean result = sdb.isValidPeriod("135", begin.getTime(), finish.getTime());
+		System.out.println(result);
+
+		client.close();
+	}
+
 	public static String DB_NAME = "tasks-monitor";
 	public static String COL_NAME = "sprints";
 
@@ -34,6 +60,22 @@ public class SprintsDB {
 		if( id == null ) return false;
 		Bson filter = Filters.eq("id", id);
 		return getCollection().count(filter) > 0;
+	}
+
+	public boolean isValidPeriod(String boardId, Date begin, Date finish){
+		List<Bson> query = Arrays.asList(
+			Aggregates.match(Filters.eq("boardId", boardId)),
+			Aggregates.group(null, Accumulators.max("lastDate", "$finishDate")));
+
+		Document doc = getCollection().aggregate(query).first();
+		if( doc != null && doc.get("lastDate") != null ){
+			Date last = doc.getDate("lastDate");
+			return last.compareTo(begin) <= 0;
+		}
+
+		System.out.println(doc);
+
+		return true;
 	}
 
 	public <T> String addSprint(T data, DocumentConverter<T> converter){
