93f58712108b7d053356b28f03aed0f2f1b5e988
koike-marshmallow <taka.vao.321@gmail.com>
2016-09-24 13:46:03 +0900

    データ追加にエラーチェックを追加

--- src/jp/ac/oit/igakilab/tasks/db/SprintsDB.java
+++ src/jp/ac/oit/igakilab/tasks/db/SprintsDB.java
@@ -16,26 +16,41 @@ import com.mongodb.client.model.Accumulators;
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
 
+import jp.ac.oit.igakilab.tasks.db.converters.SprintDocumentConverter;
+import jp.ac.oit.igakilab.tasks.sprints.Sprint;
 import jp.ac.oit.igakilab.tasks.util.RandomIdGenerator;
 
 public class SprintsDB {
+	public class SprintsDBEditException extends DBEditException{
+		public static final int INVALID_PERIOD = 2001;
+
+		public SprintsDBEditException(int code, String msg){
+			super(code, msg);
+		}
+	}
+
 	public static void main(String[] args){
 		MongoClient client = TasksMongoClientBuilder.createClient();
 		SprintsDB sdb = new SprintsDB(client);
-		/*Sprint spr = new Sprint();
-		spr.setBoardId("52e");
+		Sprint spr = new Sprint();
+		spr.setBoardId("135");
 		Calendar cal = Calendar.getInstance();
-		cal.set(2016, 8, 8);
+		cal.set(2016, 8, 19);
 		spr.setBeginDate(cal.getTime());
-		cal.set(2016, 8, 15);
+		cal.set(2016, 8, 29);
 		spr.setFinishDate(cal.getTime());
-		sdb.addSprint(spr, new SprintDocumentConverter());*/
-		Calendar begin = Calendar.getInstance();
-		begin.set(2016, 8, 15);
-		Calendar finish = Calendar.getInstance();
-		finish.set(2016, 8, 22);
-		boolean result = sdb.isValidPeriod("135", begin.getTime(), finish.getTime());
-		System.out.println(result);
+
+		String id = null;
+		try{
+			id = sdb.addSprint(spr, new SprintDocumentConverter());
+		}catch(DBEditException e0){
+			System.out.println(e0.getMessage());
+		}
+		System.out.println(id);
+
+
+
+
 
 		client.close();
 	}
@@ -63,6 +78,13 @@ public class SprintsDB {
 	}
 
 	public boolean isValidPeriod(String boardId, Date begin, Date finish){
+		if( boardId == null || begin == null || finish == null ){
+			return false;
+		}
+		if( begin.compareTo(finish) > 0 ){
+			return false;
+		}
+
 		List<Bson> query = Arrays.asList(
 			Aggregates.match(Filters.eq("boardId", boardId)),
 			Aggregates.group(null, Accumulators.max("lastDate", "$finishDate")));
@@ -78,14 +100,26 @@ public class SprintsDB {
 		return true;
 	}
 
-	public <T> String addSprint(T data, DocumentConverter<T> converter){
+	public <T> String addSprint(T data, DocumentConverter<T> converter)
+	throws SprintsDBEditException{
 		Document doc = converter.convert(data);
-		if( doc == null ) return null;
+		if( doc == null ) throw new SprintsDBEditException(
+			DBEditException.INVALID_DATA, "不正なデータです");
 
 		RandomIdGenerator gen = new RandomIdGenerator(RandomIdGenerator.CHARSET_HEX);
 		String newId = gen.generate(ID_LENGTH, OVERFLOW, (str ->
 			!sprintIdExists(str)));
-		if( newId == null ) return null;
+		if( newId == null ) throw new SprintsDBEditException(
+			DBEditException.ID_GEN_OVERFLOW, "idの割り当てに失敗しました");
+
+		if( !isValidPeriod(
+			doc.getString("boardId"),
+			doc.getDate("beginDate"),
+			doc.getDate("finishDate"))
+		){
+			throw new SprintsDBEditException(
+				SprintsDBEditException.INVALID_PERIOD, "不正な期間です");
+		}
 
 		doc.append("id", newId);
 		getCollection().insertOne(doc);
