f8decbf33b9f53e314a241890a1738d23fc48f3e
koike-marshmallow <taka.vao.321@gmail.com>
2016-09-30 18:41:57 +0900

    TrelloApiを変更

--- src/jp/ac/oit/igakilab/tasks/trello/TrelloApi.java
+++ src/jp/ac/oit/igakilab/tasks/trello/TrelloApi.java
@@ -1,10 +1,7 @@
 package jp.ac.oit.igakilab.tasks.trello;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -15,17 +12,17 @@ import jp.ac.oit.igakilab.tasks.util.HttpRequest;
 import jp.ac.oit.igakilab.tasks.util.HttpResponse;
 
 
-public class TrelloApi {
+public class TrelloApi<T>{
 	public static void main(String[] args){
 		String ak = "67ad72d3feb45f7a0a0b3c8e1467ac0b";
-		String at =  "268c74e1d0d1c816558655dbe438bb77bcec6a9cd205058b85340b3f8938fd65";
-		TrelloApi api = new TrelloApi(ak, at);
+		String at = "268c74e1d0d1c816558655dbe438bb77bcec6a9cd205058b85340b3f8938fd65";
+		TrelloApi<Object> api = new TrelloApi<Object>(ak, at, new SimpleJsonResponseTextParser());
 
-		JSONObject obj = (JSONObject)api.get("/1/members/me");
+		JSONObject obj = (JSONObject)api.rget("/1/members/me").getData();
 		System.out.println(obj.toString());
 		System.out.println("ユーザーID: " + obj.get("id"));
 
-		JSONArray boards = (JSONArray)api.get("/1/members/me/boards");
+		JSONArray boards = (JSONArray)api.rget("/1/members/me/boards").getData();
 		System.out.println(boards.toString());
 		for(Object t : boards){
 			JSONObject board = (JSONObject)t;
@@ -40,46 +37,39 @@ public class TrelloApi {
 		*/
 	}
 
-	public interface TrelloApiErrorHandler{
-		public void onHttpNG(int status, HttpResponse response);
-		public void onException(Exception e0);
+	public interface ResponseTextParser<T>{
+		public T parse(String responseText);
 	}
 
-	public static class SimpleErrorHandler
-	implements TrelloApiErrorHandler{
+	public static class SimpleJsonResponseTextParser implements ResponseTextParser<Object>{
 		@Override
-		public void onHttpNG(int status, HttpResponse response){
-			System.err.println("TrelloApi[HTTP-NG] status:" + status);
-		}
-		@Override
-		public void onException(Exception e0){
-			e0.printStackTrace();
+		public Object parse(String responseText){
+			Object parsed = null;
+			JSONParser parser = new JSONParser();
+			try{
+				parsed = parser.parse(responseText);
+			}catch(ParseException e0){
+				return null;
+			}
+			return parsed;
 		}
 	}
 
-	public static class Parameters {
-		Map<String,String> params;
+	public static class TrelloResult<T>{
+		private String errorMessage;
+		private T data;
 
-		public Parameters(){
-			params = new HashMap<String,String>();
+		TrelloResult(T data, String errMsg){
+			errorMessage = errMsg;
+			this.data = data;
 		}
 
-		public Parameters setParameter(String key, String value){
-			params.put(key, value);
-			return this;
+		public String getErrorMessage() {
+			return errorMessage;
 		}
 
-		public Parameters setParameters(Map<String,String> map){
-			for(String key : map.keySet()) setParameter(key, map.get(key));
-			return this;
-		}
-
-		public String getParameter(String key){
-			return params.get(key);
-		}
-
-		public Set<String> keySet(){
-			return params.keySet();
+		public T getData() {
+			return data;
 		}
 	}
 
@@ -88,114 +78,156 @@ public class TrelloApi {
 
 	private String apiKey;
 	private String apiToken;
-	private TrelloApiErrorHandler errorHandler;
+	private ResponseTextParser<T> parser;
 
-	public TrelloApi(String apiKey, String apiToken){
+	public TrelloApi(String apiKey, String apiToken, ResponseTextParser<T> parser){
 		this.apiKey = apiKey;
 		this.apiToken = apiToken;
+		this.parser = parser;
 	}
 
-	public void setErrorHandler(TrelloApiErrorHandler handler){
-		errorHandler = handler;
-	}
-
-	private HttpRequest createDefaultHttpRequest(String method, String url, Parameters params){
+	private HttpRequest createDefaultHttpRequest(String method, String url, Map<String,String> params){
 		HttpRequest request = new HttpRequest(method, URL_HEAD + url);
 		request.setParameter("key", apiKey)
 			.setParameter("token", apiToken);
 		if( params != null ){
-			for(String key : params.keySet()){
-				request.setParameter(key, params.getParameter(key));
-			}
+			params.forEach((key, value) -> request.setParameter(key, value));
 		}
 		return request;
 	}
 
-	private Object parseJSON(String json)
-	throws ParseException{
-		JSONParser parser = new JSONParser();
-		return parser.parse(json);
-	}
-
-	private Object sendRequestAndParseJson(String method, String url, Parameters params){
+	private T sendRequestAndParseJson(String method, String url, Map<String,String> params)
+	throws TrelloApiConnectionFailedException{
+		//HTTPリクエストの初期化
 		HttpRequest request = createDefaultHttpRequest(method, url, params);
+
+		// DEBUG リクエストを表示
 		if( DEBUG ){
 			System.out.println("TrelloApi[debug]: send:" + method + " " + url);
-			for(String key : params.keySet()){
-				System.out.println("TrelloApi[debug]: send:param " +
-					key + "=" + params.getParameter(key));
-			}
+			params.forEach((key, value) ->
+				System.out.println("TrelloApi[debug]: send:param " + key + " = " + value));
 		}
 
+		//APIに接続、情報を取得
 		HttpResponse response = null;
 		try{
 			response = request.sendRequest();
 		}catch(IOException e0){
-			if( errorHandler != null ){
-				errorHandler.onException(e0);
-			}
+			throw new TrelloApiConnectionFailedException(0, false, e0.getMessage());
 		}
 
-		if( response != null ){
-			if( DEBUG ){
-				System.out.println("TrelloApi[debug]: receive:" + response.getStatus());
-				System.out.println("TrelloApi[debug]: receive:" + response.getResponseText());
-				for(Entry<String,String> entry : response.getHeaders().entrySet()){
-					System.out.format("  %s: %s\n", entry.getKey(), entry.getValue());
-				}
-			}
-			if( response.getStatus() == HttpRequest.HTTP_OK ){
-				Object replyJson;
-				try{
-					replyJson = parseJSON(response.getResponseText());
-				}catch(ParseException e0){
-					if( errorHandler != null ){
-						errorHandler.onException(e0);
-					}
-					return null;
-				}
-				return replyJson;
-			}else{
-				if( errorHandler != null ){
-					errorHandler.onHttpNG(response.getStatus(), response);
-				}
-				return null;
-			}
-		}else{
-			return null;
+		// DEBUG レスポンスを表示
+		if( DEBUG ){
+			System.out.println("TrelloApi[debug]: receive:" + response.getStatus());
+			System.out.println("TrelloApi[debug]: receive:" + response.getResponseText());
+			response.getHeaders().forEach((key, value) ->
+				System.out.format("  %s: %s\n", key, value));
 		}
+
+		//ステータスコード確認
+		if( response.getStatus() != HttpRequest.HTTP_OK ){
+			throw new TrelloApiConnectionFailedException(
+				response.getStatus(), true, "HTTP接続エラー: " + response.getStatus());
+		}
+
+		//データを変換
+		T data = parser.parse(response.getResponseText());
+		if( data == null ){
+			throw new TrelloApiConnectionFailedException(0, false, "データ変換エラー");
+		}
+
+		return data;
 	}
 
-	public Object get(String url){
+	public T get(String url)
+	throws TrelloApiConnectionFailedException{
 		return get(url, null);
 	}
 
-	public Object get(String url, Parameters params){
+	public T get(String url, Map<String,String> params)
+	throws TrelloApiConnectionFailedException{
 		return sendRequestAndParseJson("GET", url, params);
 	}
 
-	public Object post(String url){
+	public TrelloResult<T> rget(String url){
+		return rget(url, null);
+	}
+
+	public TrelloResult<T> rget(String url, Map<String,String> params){
+		T data;
+		try{ data = get(url, params);
+		}catch(TrelloApiConnectionFailedException e0){
+			return new TrelloResult<T>(null, e0.getMessage());
+		}
+		return new TrelloResult<T>(data, null);
+	}
+
+	public T post(String url)
+	throws TrelloApiConnectionFailedException{
 		return post(url, null);
 	}
 
-	public Object post(String url, Parameters params){
+	public T post(String url, Map<String,String> params)
+	throws TrelloApiConnectionFailedException{
 		return sendRequestAndParseJson("POST", url, params);
 	}
 
-	public Object put(String url){
+	public TrelloResult<T> rpost(String url){
+		return rpost(url, null);
+	}
+
+	public TrelloResult<T> rpost(String url, Map<String,String> params){
+		T data;
+		try{ data = post(url, params);
+		}catch(TrelloApiConnectionFailedException e0){
+			return new TrelloResult<T>(null, e0.getMessage());
+		}
+		return new TrelloResult<T>(data, null);
+	}
+
+	public T put(String url)
+	throws TrelloApiConnectionFailedException{
 		return put(url, null);
 	}
 
-	public Object put(String url, Parameters params){
+	public T put(String url, Map<String,String> params)
+	throws TrelloApiConnectionFailedException{
 		return sendRequestAndParseJson("PUT", url, params);
 	}
 
-	public Object delete(String url){
+	public TrelloResult<T> rput(String url){
+		return rput(url, null);
+	}
+
+	public TrelloResult<T> rput(String url, Map<String,String> params){
+		T data;
+		try{ data = put(url, params);
+		}catch(TrelloApiConnectionFailedException e0){
+			return new TrelloResult<T>(null, e0.getMessage());
+		}
+		return new TrelloResult<T>(data, null);
+	}
+
+	public T delete(String url)
+	throws TrelloApiConnectionFailedException{
 		return delete(url, null);
 	}
 
-	public Object delete(String url, Parameters params){
+	public T delete(String url, Map<String,String> params)
+	throws TrelloApiConnectionFailedException{
 		return sendRequestAndParseJson("DELETE", url, params);
 	}
 
+	public TrelloResult<T> rdelete(String url){
+		return rdelete(url, null);
+	}
+
+	public TrelloResult<T> rdelete(String url, Map<String,String> params){
+		T data;
+		try{ data = delete(url, params);
+		}catch(TrelloApiConnectionFailedException e0){
+			return new TrelloResult<T>(null, e0.getMessage());
+		}
+		return new TrelloResult<T>(data, null);
+	}
 }
---
+++ src/jp/ac/oit/igakilab/tasks/trello/TrelloApiConnectionFailedException.java
@@ -0,0 +1,20 @@
+package jp.ac.oit.igakilab.tasks.trello;
+
+public class TrelloApiConnectionFailedException extends Exception{
+	private int statusCode;
+	private boolean isHttpError;
+
+	TrelloApiConnectionFailedException(int statusCode, boolean isHttpError, String msg){
+		super(msg);
+		this.statusCode = statusCode;
+		this.isHttpError = isHttpError;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public boolean isHttpError() {
+		return isHttpError;
+	}
+}
