2ab8f5546083f5d2740bf2664214cb5e0cac5fb3
koike-marshmallow <taka.vao.321@gmail.com>
2016-10-01 00:02:28 +0900

    CardEditorを追加

---
+++ src/jp/ac/oit/igakilab/tasks/trello/TrelloDateFormat.java
@@ -0,0 +1,18 @@
+package jp.ac.oit.igakilab.tasks.trello;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+public class TrelloDateFormat extends SimpleDateFormat{
+	public static String PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
+
+	public TrelloDateFormat(){
+		super(PATTERN);
+		setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
+
+	public void setTimeZone(TimeZone zone)
+	throws RuntimeException{
+		throw new RuntimeException("このインスタンスではタイムゾーンの変更を禁止しています");
+	}
+}
