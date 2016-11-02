026e5946f3bfddd94f96d4cfc295a7d29d7742ae
koike-marshmallow <taka.vao.321@gmail.com>
2016-11-02 22:49:29 +0900

    リザルトデータの受け取りに成功

--- src/jp/ac/oit/igakilab/tasks/dwr/SprintHistory.java
+++ src/jp/ac/oit/igakilab/tasks/dwr/SprintHistory.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import com.mongodb.MongoClient;
 
+import jp.ac.oit.igakilab.tasks.db.SprintResultsDB;
+import jp.ac.oit.igakilab.tasks.db.SprintsDB;
 import jp.ac.oit.igakilab.tasks.db.TasksMongoClientBuilder;
 import jp.ac.oit.igakilab.tasks.db.TrelloBoardActionsDB;
+import jp.ac.oit.igakilab.tasks.db.converters.SprintDocumentConverter;
+import jp.ac.oit.igakilab.tasks.db.converters.SprintResultDocumentConverter;
 import jp.ac.oit.igakilab.tasks.db.converters.TrelloActionDocumentParser;
 import jp.ac.oit.igakilab.tasks.dwr.forms.SprintHistoryForms;
-import jp.ac.oit.igakilab.tasks.dwr.forms.SprintHistoryForms.SprintList;
 import jp.ac.oit.igakilab.tasks.sprints.Sprint;
 import jp.ac.oit.igakilab.tasks.sprints.SprintManager;
 import jp.ac.oit.igakilab.tasks.sprints.SprintResult;
@@ -18,7 +21,7 @@ import jp.ac.oit.igakilab.tasks.trello.api.TrelloApi;
 import jp.ac.oit.igakilab.tasks.trello.model.TrelloActionsBoard;
 
 public class SprintHistory {
-	public SprintList getSprintList(String boardId){
+	public SprintHistoryForms.SprintList getSprintList(String boardId){
 		//各種クライアントを初期化
 		MongoClient client = TasksMongoClientBuilder.createClient();
 		TrelloApi<Object> api = TasksTrelloClientBuilder.createApiClient();
@@ -35,9 +38,46 @@ public class SprintHistory {
 		//リストを取得
 		List<Sprint> sl = sm.getSprintsByBoardId(boardId);
 		List<SprintResult> rl = sm.getSprintResultsByBoardId(boardId);
+		System.out.println(sl.size() + " " + sl.toString());
+		System.out.println(rl.size() + " " + rl.toString());
 
 		client.close();
 		//リストを変換して返却
 		return SprintHistoryForms.SprintList.getInstance(bdata, sl, rl);
+	}
+
+	public SprintHistoryForms.SprintResultData getSprintResult(String sprintId)
+	throws ExcuteFailedException{
+		//各種クライアントを初期化
+		MongoClient client = TasksMongoClientBuilder.createClient();
+
+		//dbインスタンスを初期化
+		SprintsDB sdb = new SprintsDB(client);
+		SprintResultsDB srdb = new SprintResultsDB(client);
+
+		//スプリントを取得
+		Sprint sprint = sdb.getSprintById(sprintId, new SprintDocumentConverter());
+		if( sprint == null ){
+			client.close();
+			throw new ExcuteFailedException("スプリントがみつかりません");
+		}
+
+		//スプリントリザルトを取得
+		SprintResult result = srdb.getSprintResultBySprintId(
+			sprint.getId(), new SprintResultDocumentConverter());
+		if( result == null ){
+			client.close();
+			throw new ExcuteFailedException("スプリントリザルトが見つかりません");
+		}
+
+		//ボード情報を取得
+		TrelloBoardActionsDB adb = new TrelloBoardActionsDB(client);
+		TrelloActionsBoard board = new TrelloActionsBoard();
+		board.addActions(adb.getTrelloActions(sprint.getBoardId(), new TrelloActionDocumentParser()));
+		board.build();
+
+		client.close();
+		//リストを変換して返却
+		return SprintHistoryForms.SprintResultData.getInstance(sprint, result, board);
 	}
 }
--- src/jp/ac/oit/igakilab/tasks/dwr/forms/SprintHistoryForms.java
+++ src/jp/ac/oit/igakilab/tasks/dwr/forms/SprintHistoryForms.java
@@ -7,8 +7,106 @@ import java.util.List;
 import jp.ac.oit.igakilab.tasks.sprints.Sprint;
 import jp.ac.oit.igakilab.tasks.sprints.SprintResult;
 import jp.ac.oit.igakilab.tasks.trello.TrelloBoardData;
+import jp.ac.oit.igakilab.tasks.trello.model.TrelloBoard;
+import jp.ac.oit.igakilab.tasks.trello.model.TrelloCard;
 
 public class SprintHistoryForms {
+	public static class SprintResultData{
+		protected static TrelloCardForm getTrelloCardForm
+		(TrelloBoard board, String cardId, List<String> memberIds){
+			TrelloCard card = board.getCardById(cardId);
+
+			TrelloCardForm cf = null;
+			if( card != null ){
+				cf = TrelloCardForm.getInstance(card);
+			}else{
+				cf = new TrelloCardForm();
+				cf.setId(cardId);
+			}
+			cf.setMemberIds(memberIds.toArray(new String[memberIds.size()]));
+
+			return cf;
+		}
+
+		public static SprintResultData getInstance(Sprint spr, SprintResult res, TrelloBoard board){
+			if( spr == null || res == null || board == null ) return null;
+
+			SprintResultData form = new SprintResultData();
+			form.setId(spr.getId());
+			form.setBeginDate(spr.getBeginDate());
+			form.setFinishDate(spr.getFinishDate());
+			form.setClosedDate(spr.getClosedDate());
+
+			List<TrelloCardForm> rtmp = new ArrayList<TrelloCardForm>();
+			res.getRemainedCards().forEach(c ->
+				rtmp.add(getTrelloCardForm(board, c.getCardId(), c.getMemberIds())));
+			form.setRemainedCards(rtmp);
+			List<TrelloCardForm> ftmp = new ArrayList<TrelloCardForm>();
+			res.getFinishedCards().forEach(c ->
+				ftmp.add(getTrelloCardForm(board, c.getCardId(), c.getMemberIds())));
+			form.setRemainedCards(ftmp);
+
+			form.setBoardData(TrelloBoardDataForm.getInstance(board));
+
+			return form;
+		}
+
+
+		private String id;
+
+		private Date beginDate;
+		private Date finishDate;
+
+		private Date closedDate;
+		private List<TrelloCardForm> remainedCards;
+		private List<TrelloCardForm> finishedCards;
+
+		private TrelloBoardDataForm boardData;
+
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public Date getBeginDate() {
+			return beginDate;
+		}
+		public void setBeginDate(Date beginDate) {
+			this.beginDate = beginDate;
+		}
+		public Date getFinishDate() {
+			return finishDate;
+		}
+		public void setFinishDate(Date finishDate) {
+			this.finishDate = finishDate;
+		}
+		public Date getClosedDate() {
+			return closedDate;
+		}
+		public void setClosedDate(Date closedDate) {
+			this.closedDate = closedDate;
+		}
+		public List<TrelloCardForm> getRemainedCards() {
+			return remainedCards;
+		}
+		public void setRemainedCards(List<TrelloCardForm> remainedCards) {
+			this.remainedCards = remainedCards;
+		}
+		public List<TrelloCardForm> getFinishedCards() {
+			return finishedCards;
+		}
+		public void setFinishedCards(List<TrelloCardForm> finishedCards) {
+			this.finishedCards = finishedCards;
+		}
+		public TrelloBoardDataForm getBoardData() {
+			return boardData;
+		}
+		public void setBoardData(TrelloBoardDataForm boardData) {
+			this.boardData = boardData;
+		}
+	}
+
 	public static class SprintList{
 		public static SprintList getInstance(TrelloBoardData bdata, List<SprintData> sprints){
 			SprintList form = new SprintList();
@@ -57,10 +155,13 @@ public class SprintHistoryForms {
 
 			if( res == null ) return form;
 			//スプリントの結果情報を格納
+			form.setClosedDate(spr.getClosedDate());
 			List<TrelloCardMembersForm> rtmp = new ArrayList<TrelloCardMembersForm>();
 			res.getRemainedCards().forEach(c -> rtmp.add(TrelloCardMembersForm.getInstance(c)));
+			form.setRemainedCards(rtmp);
 			List<TrelloCardMembersForm> ftmp = new ArrayList<TrelloCardMembersForm>();
 			res.getFinishedCards().forEach(c -> ftmp.add(TrelloCardMembersForm.getInstance(c)));
+			form.setFinishedCards(ftmp);
 
 			//返却
 			return form;
