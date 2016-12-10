ee0e09eb43700c70c53de162e4ae0e100982cb07
koike-marshmallow <taka.vao.321@gmail.com>
2016-12-10 22:42:15 +0900

    dashboardにタイムラインを連携させた

--- WebContent/dashboard.js
+++ WebContent/dashboard.js
@@ -65,33 +65,7 @@ function showDashBoard(data){
 
 	// *****
 	// 進捗グラフの表示
-	$(".sprint-cards").empty();
-	for(var i=0; i<data.sprintCards.length; i++){
-		var card = data.sprintCards[i];
-
-		//ラベル部分を作成
-		var $label = $("<ol></ol>").append(
-			$("<li></li>").append(
-				$("<h2></h2>").append(card.name)
-			)
-		).css("margin", "0 auto");
-
-		//タイムライングラフを作成
-		var $timeline = createTaskTimeline("sc" + i,
-			data.beginDate, card.movedDoingAt, card.movedDoneAt, data.finishDate);
-
-		//左にタスク名、右にタイムライングラフを格納して、sprint-cardsに追加
-		$(".sprint-cards").append(
-			$("<div></div>").addClass("row").append(
-				$("<section></section>").addClass("cd-horizontal-timeline").append(
-					$("<div></div>").addClass("col-md-3").append($label),
-					$("<div></div>").addClass("col-md-9").append($timeline)
-				)
-			)
-		);
-	}
-	//タイムラインを初期化
-	$(".cd-horizontal-timeline").initTimeline();
+	drawSprintTasksTimeline(data.sprintCards, data.beginDate, data.finishDate);
 
 	// *****
 	// かんばんの表示
@@ -106,63 +80,169 @@ function showDashBoard(data){
 /*
  * 進捗グラフのグラフ部分の要素を作成します
  */
-function createTaskTimeline(id, dBegin, dDoing, dDone, dFinish){
-	//日付フォーマット作成関数
-	var formatDate = function(date){
-		return date.getFullYear() + "/" + (date.getMonth()+1) + "/" + date.getDate();
-	};
-	//ol要素を作成
-	var $ol = $("<ol></ol>").attr("id", id);
-	//doing,doneの文字列表現を作成
-	var dDoingStr = Util.isNull(dDoing) ? "null" : formatDate(dDoing);
-	var dDoneStr = Util.isNull(dDone) ? "null" : formatDate(dDone);
+//function createTaskTimeline(id, dBegin, dDoing, dDone, dFinish){
+//	//日付フォーマット作成関数
+//	var formatDate = function(date){
+//		return date.getFullYear() + "/" + (date.getMonth()+1) + "/" + date.getDate();
+//	};
+//	//ol要素を作成
+//	var $ol = $("<ol></ol>").attr("id", id);
+//	//doing,doneの文字列表現を作成
+//	var dDoingStr = Util.isNull(dDoing) ? "null" : formatDate(dDoing);
+//	var dDoneStr = Util.isNull(dDone) ? "null" : formatDate(dDone);
+//
+//	//開始日から終了日まで繰り返し
+//	var dtmp = new Date(dBegin.getTime());
+//	var selflg = true;
+//	while( dtmp.getTime() <= dFinish.getTime() ){
+//		//data-dateの文字列表現を生成、doing,doneの表現と比較
+//		var dtmpStr = formatDate(dtmp);
+//		var label = Util.formatDate(dtmp, "MM/DD");
+//		var clazz = "";
+//
+//		//doing,doneの表現と比較、それぞれの処理
+//		if( dtmpStr == dDoneStr ){
+//			clazz = "done";
+//			label += "<br/>Done登録";
+//		}else if( dtmpStr == dDoingStr ){
+//			clazz = "doing";
+//			label += "<br/>Doing登録";
+//		}
+//
+//		//初回の要素にselectedクラスを付加する
+//		if( selflg ){
+//			clazz = clazz + " selected";
+//			selflg = false;
+//		}
+//
+//		//要素を生成し追加
+//		$ol.append($("<li></li>").append(
+//			$("<a></a>").attr("href", "#0")
+//				.attr("data-date", dtmpStr)
+//				.addClass(clazz)
+//				.append(label)
+//		));
+//
+//
+//		//日付をインクリメント
+//		dtmp.setDate(dtmp.getDate() + 1);
+//	}
+//
+//	//ラッパーに入れて返却
+//	return $("<div></div>").addClass("timeline").append(
+//		$("<div></div>").addClass("events-wrapper").append(
+//			$("<div></div>").addClass("events").append($ol,
+//				$("<span></span>").attr("id", id)
+//					.addClass("filling-line")
+//					.attr("aria-hidden", "true"))
+//		)
+//	);
+//}
 
-	//開始日から終了日まで繰り返し
-	var dtmp = new Date(dBegin.getTime());
-	var selflg = true;
-	while( dtmp.getTime() <= dFinish.getTime() ){
-		//data-dateの文字列表現を生成、doing,doneの表現と比較
-		var dtmpStr = formatDate(dtmp);
-		var label = Util.formatDate(dtmp, "MM/DD");
-		var clazz = "";
 
-		//doing,doneの表現と比較、それぞれの処理
-		if( dtmpStr == dDoneStr ){
-			clazz = "done";
-			label += "<br/>Done登録";
-		}else if( dtmpStr == dDoingStr ){
-			clazz = "doing";
-			label += "<br/>Doing登録";
+/*
+ * タスクごとのタイムラインを描画します
+ * beginDateには開始時刻、dataには下記のフォーマットを指定します
+ * [{name:<タスク名>, listUpdates:<<analyzeListTimelineに依存>>},..]
+ */
+function drawSprintTasksTimeline(data, beginDate, finishDate, destId){
+	//データテーブルを初期化
+	var dataTable = new google.visualization.DataTable();
+	dataTable.addColumn({type: "string", id: "TaskName"});
+	dataTable.addColumn({type: "string", id: "Status"});
+	dataTable.addColumn({type: "date", id: "Start"});
+	dataTable.addColumn({type: "date", id: "End"});
+
+	//タイムラインの終了時刻を計算
+	var now = new Date();
+	if( finishDate.getTime() < now.getTime() ){
+		finishDate = now;
+	}else{
+		finishDate.setDate(finishDate.getDate() + 1);
+		finishDate.setHours(0);
+		finishDate.setMinutes(0);
+		finishDate.setSeconds(0);
+	}
+	console.log("finishDate");
+	console.log(finishDate);
+
+	//カードごとにタイムラインを追加
+	data.forEach(function(card, idx, ary){
+		//タイムラインを解析
+		var lines = analyzeListTimeline(card.listUpdates, beginDate, now);
+
+		//終了時刻よりも前のデータしかない場合は空のデータを追加
+		if( lines[lines.length-1].end.getTime() < finishDate ){
+			lines.push({
+				data:{name:"unknown", type:"unknown"},
+				start:lines[lines.length-1].end, end:finishDate
+			});
 		}
 
-		//初回の要素にselectedクラスを付加する
-		if( selflg ){
-			clazz = clazz + " selected";
-			selflg = false;
-		}
+		//タイムラインを追加
+		lines.forEach(function(move){
+			var label = move.data.type == "todo" ? "ToDo" :
+				move.data.type == "doing" ? "Doing" :
+				move.data.type == "done"  ? "Done" : "unknown";
 
-		//要素を生成し追加
-		$ol.append($("<li></li>").append(
-			$("<a></a>").attr("href", "#0")
-				.attr("data-date", dtmpStr)
-				.addClass(clazz)
-				.append(label)
-		));
+			dataTable.addRow([card.name, label, move.start, move.end]);
+		});
+	});
+
+	//オプションを生成
+	var options = {width: "100%"};
+
+	//描画
+	var chart = new google.visualization.Timeline(document.getElementById(destId || "n-timeline"));
+	chart.draw(dataTable);
+}
 
 
-		//日付をインクリメント
-		dtmp.setDate(dtmp.getDate() + 1);
+/*
+ * リスト移動履歴からカードがリストに到達した時間と離脱した時間を解析します
+ * fStart,fEndには日時を、listMovementsには下記のフォーマットを指定します。
+ * [{movedAt:<移動が発生した日時>,name:<移動先リスト名>,type:<移動先リストタイプ>},..]
+ */
+function analyzeListTimeline(listMovements, fStart, fEnd){
+	var result = [];
+	var tmp = null;
+	var cnt = 0;
+
+	//初期位置を探索
+	while( cnt < listMovements.length && listMovements[cnt].movedAt <= fStart ){
+		//console.log("skip");
+		//console.log(listMovements[cnt]);
+		cnt++;
 	}
 
-	//ラッパーに入れて返却
-	return $("<div></div>").addClass("timeline").append(
-		$("<div></div>").addClass("events-wrapper").append(
-			$("<div></div>").addClass("events").append($ol,
-				$("<span></span>").attr("id", id)
-					.addClass("filling-line")
-					.attr("aria-hidden", "true"))
-		)
-	);
+	//初期位置を設定
+	if( cnt > 0 ){
+		//console.log("init configure");
+		//console.log(listMovements[cnt-1]);
+		tmp = {data: listMovements[cnt-1], start: fStart};
+	}else{
+		tmp = {data: {name:"unknown", type:"unknown"}, start: fStart};
+	}
+
+	//移動履歴を探索
+	while( cnt < listMovements.length && listMovements[cnt].movedAt < fEnd ){
+		//console.log("analyze");
+		//console.log(listMovements[cnt]);
+
+		var move = listMovements[cnt];
+		tmp.end = move.movedAt;
+		result.push(tmp);
+
+		tmp = {data: move, start: move.movedAt};
+		cnt++;
+	}
+
+	//終端処理
+
+	tmp.end = fEnd;
+	result.push(tmp);
+
+	return result;
 }
 
 /*
