1a33966e84e03e6346efa0418e490c8b015cee4f
Koike Takaaki <taka.vao.321@gmail.com>
2016-12-20 11:23:25 +0900

    taskNotifyを作成

--- src/jp/ac/oit/igakilab/tasks/hubot/HubotTaskNotify.java
+++ src/jp/ac/oit/igakilab/tasks/hubot/HubotTaskNotify.java
@@ -1,21 +1,29 @@
 package jp.ac.oit.igakilab.tasks.hubot;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import jp.ac.oit.igakilab.tasks.model.Card;
 import jp.ac.oit.igakilab.tasks.util.HttpRequest;
 import jp.ac.oit.igakilab.tasks.util.HttpResponse;
 
 public class HubotTaskNotify extends HubotSendMessage{
+	public static void main(String[] args){
+		List<String> cards = Arrays.asList("カード1", "#33 カード2", "カード3 やで");
+		HubotTaskNotify notifer = new HubotTaskNotify("");
+		try{notifer.taskNotify("test", "あいうえお", cards);}
+		catch(IOException e0){}
+	}
+
 	public HubotTaskNotify(String hubotUrl){
 		super(hubotUrl);
 	}
 
 	@SuppressWarnings("unchecked")
-	public HttpResponse taskNotify(String dest, String message, List<Card> cards)
+	public HttpResponse taskNotify(String room, String message, List<String> cardNames)
 	throws IOException{
 		//リクエストを生成
 		HttpRequest request = new HttpRequest("POST", hubotUrl + "/hubot/task_notify");
@@ -23,11 +31,16 @@ public class HubotTaskNotify extends HubotSendMessage{
 
 		//送信ボディを構成
 		JSONObject body = new JSONObject();
-		body.put("room", dest);
+		body.put("room", room);
 		body.put("message", message);
+		JSONArray cards = new JSONArray();
+		cards.addAll(cardNames);
+		body.put("cards", cards);
+		System.out.println(body.toJSONString());
 
 		//リクエストを送信
-		HttpResponse res = request.sendRequest(body.toJSONString());
+		//HttpResponse res = request.sendRequest(body.toJSONString());
+		HttpResponse res = null;
 
 		return res;
 	}
